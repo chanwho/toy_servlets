042dbb2f68d2ee9d4894041bb5c0bc65ccfb8f99
chanwho <anxi0015@gmail.com>
2022-12-15 09:29:51 +0900

    first commit

---
+++ src/test/java/com/chanwho/toy_servlets/ToyServletsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanwho.toy_servlets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyServletsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
